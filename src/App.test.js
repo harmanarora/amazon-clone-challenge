18dbc16b267172b260929588c3d30c8a4da057ba
harmanarora <mails4harman@gmail.com>
2021-03-29 04:40:18 +0530

    test updated

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/cart/i);
+  const linkElement = screen.findAllByText(/cart/i);
   expect(linkElement).toBeInTheDocument();
 });
